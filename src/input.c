5588ecbffa7c1d7ab453e94405ada6aaf1bf6401
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2018-05-28 21:36:23 +0200

    Files splited.

--- src/input.c
+++ src/input.c
@@ -1,15 +1,21 @@
 #include "input.h"
 
+
 int8_t unixGetch(void) // https://stackoverflow.com/questions/12710582/
 {
 	int8_t key;
 	struct termios oldt,newt;
-	tcgetattr( STDIN_FILENO, &oldt );
+	tcgetattr(STDIN_FILENO, &oldt);
 	newt = oldt;
-	newt.c_lflag &= ~( ICANON | ECHO );
-	tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 	key = getchar();
-	tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 	return key;
 }
-
+/*
+int8_t getKey(void)
+{
+	
+}
+*/
--- src/input.h
+++ src/input.h
@@ -1,9 +1,11 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include <stdint.h>
 #include <unistd.h>
 #include <termios.h>
 
 int8_t unixGetch(void);
+int8_t getKey(void);
 #endif
 
