8043f4edb2cf1996bccce3a0b9d87522a15d74b3
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2024-11-12 11:58:03 +0100

    Remove ambigous typing
    https://stackoverflow.com/a/1171855

--- src/input.c
+++ src/input.c
@@ -2,10 +2,10 @@
 
 char input__getch(void)
 {
-    const unsigned int canonical_mode_on = ICANON;
-    const unsigned int echo_input        = ECHO;
-    const unsigned int enable_sigs       = ISIG;
-    const unsigned int enable_xon        = IXON;
+    const unsigned canonical_mode_on = ICANON;
+    const unsigned echo_input        = ECHO;
+    const unsigned enable_sigs       = ISIG;
+    const unsigned enable_xon        = IXON;
 
     struct termios old_term_params;
     struct termios new_term_params;
